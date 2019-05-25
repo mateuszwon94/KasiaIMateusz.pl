1f8280f1e6597a823fd577178ccfdbf1de8123b2
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-25 11:17:55 +0200

    Display just string

--- KasiaIMateusz/urls.py
+++ KasiaIMateusz/urls.py
@@ -19,8 +19,9 @@ from datetime import datetime
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home),
 ]
