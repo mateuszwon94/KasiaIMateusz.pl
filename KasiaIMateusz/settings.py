576a733b3c8827d00547523ba8e789dc1f60d196
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-27 22:31:18 +0200

    Add jobs and blog apps

--- KasiaIMateusz/settings.py
+++ KasiaIMateusz/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = [
+    "Jobs",
+    "Blog",
     # Add your apps here to enable them
     'django.contrib.admin',
     'django.contrib.auth',
