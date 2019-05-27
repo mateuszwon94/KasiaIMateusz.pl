0287fb33a5ed2905fc0c297de1a961b9797a2ccf
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-27 22:53:28 +0200

    First model

--- Jobs/models.py
+++ Jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    image = models.ImageField(upload_to="images/") # upload image file to ./media/images/ directory
+    summary = models.CharField(max_length=200)
