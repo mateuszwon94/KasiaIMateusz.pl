a33c2dbafa9b3f3ef1ecae7a1c2139dff772507d
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-27 22:50:56 +0200

    Add media root directory and url to settings

--- KasiaIMateusz/settings.py
+++ KasiaIMateusz/settings.py
@@ -113,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+MEDIA_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['media']))
+
+MEDIA_URL = "/media/"
