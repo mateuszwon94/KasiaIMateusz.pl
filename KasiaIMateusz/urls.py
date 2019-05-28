641ce2e6d87867ba5d3e4319ef0041e1cc147765
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-28 19:20:41 +0200

    Enable media to be accessed by the url

--- KasiaIMateusz/urls.py
+++ KasiaIMateusz/urls.py
@@ -19,8 +19,10 @@ from datetime import datetime
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
