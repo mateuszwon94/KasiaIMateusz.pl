d304f5008d4b4b9b95ad106c23ce77a9bec8df09
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-05-31 18:39:01 +0200

    First part of implementation blog page

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/") # upload image file to ./media/images/ directory
+
+    def summary(self):
+        return self.body[:100]
+    
+    def __str__(self):
+        return self.title
