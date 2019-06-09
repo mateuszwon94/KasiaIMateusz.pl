816a8e1db82751faaafe01efdece0a1f5c2d58f7
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-06-09 12:54:52 +0200

    First try of OneDrive embad

--- Blog/views.py
+++ Blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from KasiaIMateusz.utils import get_item_from_onedrive
 
 # Create your views here.
 
 def allblogs(request):
-    all_blogs = Blog.objects
-    return render(request, "Blog/allblogs.html", {"Blogs" : all_blogs})
+    photos = get_item_from_onedrive("/Inne/Inne")
+
+    images = []
+    for photo in photos.get_items(limit=10):
+        if photo.is_photo or photo.is_image:
+            images.append(photo.share_with_link("embed").share_link)
+
+    print(images)
+
+    return render(request, "Blog/allblogs.html", {"images" : images})
 
 def detail(request, blog_id):
     blog_object = get_object_or_404(Blog, pk=blog_id)
--- KasiaIMateusz/utils.py
+++ KasiaIMateusz/utils.py
@@ -8,7 +8,8 @@ def get_item_from_onedrive(path:str, folder=DRIVE):
         if f == "..":
             current_folder = current_folder.get_parent()
         else:
-            for item in current_folder:
+            for item in current_folder.get_items():
                 if item.name == f:
                     current_folder = item
                     break
+    return current_folder
