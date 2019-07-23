d90904af0aa437fdf656cca5a11909bcf94fb768
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2019-07-23 21:27:13 +0200

    Implementation of embed photo from OneDrive

--- Blog/views.py
+++ Blog/views.py
@@ -5,12 +5,18 @@ from KasiaIMateusz.utils import get_item_from_onedrive
 # Create your views here.
 
 def allblogs(request):
-    photos = get_item_from_onedrive("/Inne/Inne")
+    photos = get_item_from_onedrive("/Inne")
 
     images = []
     for photo in photos.get_items(limit=10):
         if photo.is_photo or photo.is_image:
-            images.append(photo.share_with_link("embed").share_link)
+            print(dir(photo))
+            print(dir(photo.share_with_link("embed")))
+            print(photo.dimensions)     # This is dimensions of photo
+            print(photo.web_url)        # This is a OneDrive url
+            images.append("https://api.onedrive.com/v1.0/shares/" + photo.share_with_link("embed").share_id + "/root/content")
+            # https://api.onedrive.com/v1.0/shares/{shareId}/root/content
+            # https://api.onedrive.com/v1.0/shares/{shareId}/root/thumbnails/0/[small/medium/large]/content
 
     print(images)
 
